d4ed50a90dcc23d50d96272cf179647505087b75
Ryan Lieu <ryanlieu@umich.edu>
2020-03-09 22:18:19 -0400

    init, simple black and white done

---
+++ content.js
@@ -0,0 +1,8 @@
+(function () {
+  var body = document.body;
+  body.style['filter'] = 'progid:DXImageTransform.Microsoft.BasicImage(grayscale=1)';
+  if (!body.style['filter']) {
+    body.style['-webkit-filter'] = 'grayscale(1)';
+    body.style['filter'] = 'grayscale(1)';
+  }
+}());
